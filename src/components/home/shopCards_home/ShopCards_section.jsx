d4f4bb45a5dc6f2460d7f7b7f5fd2d1a8df76b48
SameerZerror <sameer.zerror@gmail.com>
2024-08-10 19:04:12 +0530

    card section completed

--- src/components/home/shopCards_home/ShopCards_section.jsx
+++ src/components/home/shopCards_home/ShopCards_section.jsx
@@ -3,11 +3,340 @@ import React from "react";
 
 const ShopCards_section = () => {
   return (
-    <div className="ShopCards_section">
-      <div className="ShopCards_container">
-        <Link href={""}></Link>
-      </div>
+    // <div className="ShopCards_section">
+    <div className="ShopCards_container">
+      <Link href={""} className="shop-card_grid">
+        <div className="shop_card_cntr">
+          {/* <div className="shop_card_top"> */}
+          <div className="shop_card_img_wrapper">
+            <div className="responsive_shop_wrapper">
+              <div className="respo-shop_card-img_wrap">
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Zakary-scotch_1dark.jpg?v=1713365490&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Zakary-scotch_2dark.jpg?v=1713365490&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="shop_card_img_cntr">
+              <div className="shop_card_img_inner">
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Zakary-scotch_1dark.jpg?v=1713365490&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="shop_card_img_cover_hidden">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Zakary-scotch_2dark.jpg?v=1713365490&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* </div> */}
+          <div className="shop_card_text_cntr">
+            <h2 className="shop_card_text_brandName">Zakary</h2>
+            <span className="shop_card_tag">New</span>
+            <h3 className="shop_card_item_Name">Belted Leather Jacket</h3>
+            <div className="shop_card_price_wrap">
+              <div className="shop_card_price_cntr">
+                <span>1,545</span>
+                <span>&nbsp;EUR</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+      <Link href={""} className="shop-card_grid">
+        <div className="shop_card_cntr">
+          {/* <div className="shop_card_top"> */}
+          <div className="shop_card_img_wrapper">
+            <div className="responsive_shop_wrapper">
+              <div className="respo-shop_card-img_wrap">
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Edra-black_1.jpg?v=1713894113&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Edra-black_4.jpg?v=1713894113&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="shop_card_img_cntr">
+              <div className="shop_card_img_inner">
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Edra-black_1.jpg?v=1713894113&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="shop_card_img_cover_hidden">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Edra-black_4.jpg?v=1713894113&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* </div> */}
+          <div className="shop_card_text_cntr"></div>
+        </div>
+      </Link>
+      <Link href={""} className="shop-card_grid">
+        <div className="shop_card_cntr">
+          {/* <div className="shop_card_top"> */}
+          <div className="shop_card_img_wrapper">
+            <div className="responsive_shop_wrapper">
+              <div className="respo-shop_card-img_wrap">
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Kerri-camel_3.jpg?v=1711721968&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Kerri-camel_1.jpg?v=1711721968&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="shop_card_img_cntr">
+              <div className="shop_card_img_inner">
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Kerri-camel_3.jpg?v=1711721968&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="shop_card_img_cover_hidden">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Kerri-camel_1.jpg?v=1711721968&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* </div> */}
+          <div className="shop_card_text_cntr"></div>
+        </div>
+      </Link>
+      <Link href={""} className="shop-card_grid">
+        <div className="shop_card_cntr">
+          {/* <div className="shop_card_top"> */}
+          <div className="shop_card_img_wrapper">
+            <div className="responsive_shop_wrapper">
+              <div className="respo-shop_card-img_wrap">
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Drey-black_1.jpg?v=1710956335&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Drey-black_2.jpg?v=1710956336&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="shop_card_img_cntr">
+              <div className="shop_card_img_inner">
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Drey-black_1.jpg?v=1710956335&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="shop_card_img_cover_hidden">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Drey-black_2.jpg?v=1710956336&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* </div> */}
+          <div className="shop_card_text_cntr"></div>
+        </div>
+      </Link>
+      <Link href={""} className="shop-card_grid">
+        <div className="shop_card_cntr">
+          {/* <div className="shop_card_top"> */}
+          <div className="shop_card_img_wrapper">
+            <div className="responsive_shop_wrapper">
+              <div className="respo-shop_card-img_wrap">
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Zakary-scotch_1dark.jpg?v=1713365490&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Zakary-scotch_2dark.jpg?v=1713365490&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="shop_card_img_cntr">
+              <div className="shop_card_img_inner">
+                <div className="shop_card_img-wrapper">
+                  <div className="shop_card_img_cover">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Zakary-scotch_1dark.jpg?v=1713365490&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="shop_card_img_cover_hidden">
+                    <div className="shop_card_img_bgcover">
+                      <div className="shop_card_img-main_cntr">
+                        <img
+                          src="https://cdn.shopify.com/s/files/1/0030/2946/7203/files/Zakary-scotch_2dark.jpg?v=1713365490&width=420&height=630&crop=center"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* </div> */}
+          <div className="shop_card_text_cntr"></div>
+        </div>
+      </Link>
     </div>
+    // </div>
   );
 };
 
