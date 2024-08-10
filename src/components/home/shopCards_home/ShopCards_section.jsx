b7839ffc45d39144169194d21eefef04c19bc05b
SameerZerror <sameer.zerror@gmail.com>
2024-08-10 12:25:26 +0530

    how to nail responsive

--- src/components/home/shopCards_home/ShopCards_section.jsx
+++ src/components/home/shopCards_home/ShopCards_section.jsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const ShopCards_section = () => {
   return (
     <div className="ShopCards_section">
-      <div className="ShopCards_container"></div>
+      <div className="ShopCards_container">
+        <Link href={""}></Link>
+      </div>
     </div>
   );
 };
