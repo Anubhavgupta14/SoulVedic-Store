e03127e52d4dc0311bca07d7021bfaef46c046e4
SameerZerror <sameer.zerror@gmail.com>
2024-08-27 11:11:31 +0530

    navbar and some more change i have done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,11 +5,13 @@ import "@/styles/home.css";
 import "@/styles/navbar.css";
 import "@/styles/collections.css";
 import "@/styles/shopAll.css";
+// import Modal from "@/components/Modal";
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+
       <Footer />
     </>
   );
